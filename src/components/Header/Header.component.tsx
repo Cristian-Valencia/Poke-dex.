28c1bbf9bb6ba76b201edc79698c390f6300d547
Cristian Valencia <cristianvalencia@live.it>
2021-06-24 15:59:58 +0200

    feat(hamburger-menu) : hamburger-menu set up

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import './Header.scss';
 
 
 const Header = () => {
-    return (
-        <div>
 
-            <h2>I am the header</h2>
+    return (
+        <div className='header'>
+
+            <div className="header-title-container">
+                <h1 className='header-title'>Pokédex</h1>
+            </div>
+
+            <div className='header-hamburger-menu-container'>
+                <div className='header-hamburger-one'></div>
+                <div className='header-hamburger-two'></div>
+                <div className='header-hamburger-three'></div>
+            </div>
             
         </div>
     )
